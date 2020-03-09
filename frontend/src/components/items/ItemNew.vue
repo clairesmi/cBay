1586c1a39a513a4b598cbe9428bda899f1f7cc1c
Claire <smith.clairelouise@gmail.com>
2020-03-09 23:05:07 +0000

    new item component refactored

--- frontend/src/components/items/ItemNew.vue
+++ frontend/src/components/items/ItemNew.vue
@@ -1,47 +1,28 @@
 <template>
   <div id="item-new">
     <router-link to="/">Home</router-link>
-    <form>
-      <label>Name</label>
-      <input type="text" placeholder="Product name" v-model="item.name"/>
-      <label>Price</label>
-      <input type="number" placeholder="0.00" step="0.01" v-model="item.price"/>
-       <label>Size</label>
-      <input type="text" placeholder="Product size" v-model="item.size"/>
-      <!-- sep component -->
-      <label>Image</label>
-      <image-upload v-on:image-upload="item.image = $event"></image-upload>
-      <label>Categories</label>
-        <div>
-          <multiselect @input="handleMultiSelect"
-            v-model="value"
-            :options="categories"
-            :multiple="true"
-            track-by="name"
-            label="name"
-            placeholder="Select item categories">
-              <template slot="singleLabel" slot-scope="{ option }">{{ option.name }}</template>
-          </multiselect>
-            <pre class="categories-chosen"><code>{{ value ? value.name: null }}</code></pre>
-        </div>
-      <button @click="handleSubmit">List your Item</button>
-    </form>
+    <item-form v-if="item" 
+    :item="item" 
+    :categories="categories" 
+    :value="value"
+    @submit-clicked="handleSubmit"
+    @multi-select-input="handleMultiSelect"
+    >
+    </item-form>
   </div>
 </template>
 
 <script>
 // import external libraries
-import Vue from 'vue'
 import axios from 'axios'
-import Multiselect from 'vue-multiselect'
+import ItemForm from '../items/ItemForm.vue'
 // import child components here 
-import ImageUpload from './ImageUpload.vue'
 
 export default {
   name: "item-new",
 // register imported components here so they can be used in the render/template
   components: {
-    ImageUpload, Multiselect
+    ItemForm
   },
 
   data () {
@@ -74,12 +55,12 @@ export default {
        }
     },
 
-    handleMultiSelect(value) {
+    handleMultiSelect(updatedValue) {
       // console.log(value)
-      if (!value) {
+      if (!updatedValue) {
         return this.item = {...this.item, categories: []}
       }
-      this.item = {...this.item, categories: value.map(val => val.id)}
+      this.item = {...this.item, categories: updatedValue.map(val => val.id)}
       // console.log(this.item)
     },
 
