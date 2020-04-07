cc2d782c8ded1af0222b3c503eec6d85dd523133
Claire <smith.clairelouise@gmail.com>
2020-04-07 17:56:58 +0100

    categories filter styled

--- frontend/src/components/categories/CategoryShow.vue
+++ frontend/src/components/categories/CategoryShow.vue
@@ -2,14 +2,15 @@
   <div id="category-show" class=" flex flex-col justify-center items-center font-sans bg-red-200">
     <h1 class="animated zoomInDown category-title tracking-wide text-orange-600"
      v-if="category">{{ this.header.slice(0, 1).toUpperCase() + this.header.slice(1) }}</h1>
-       <form class="flex justify-center w-full text-gray-800">
-        <input class="mt-5 w-1/3"
+     <div class="flex w-3/6 justify-center items-center bg-white">
+       <form class="flex w-3/6">
+        <input class="w-full"
         placeholder=" search by keyword"
         v-model="searchTerm"
         @input.prevent="listingSearch"
       />
       </form>
-      <multiselect
+      <multiselect class="pr-20 w-2/4"
       v-model="selectedSizes"
       @input="listingSearch"
       :options="sizes"
@@ -18,6 +19,7 @@
       >
       <template slot="singleLabel" slot-scope="{ option }">{{ option }}</template>
       </multiselect>
+    </div>
     <div v-if="category.length" class="flex flex-row flex-wrap justify-around p-5">
       <div v-for="elem in listingSearch()" :key="elem.id" class="item-card flex flex-col p-5 bg-white m-5">
         <router-link :to="`/items/${elem.id}/`"><img :src="elem.image" /></router-link>
@@ -100,14 +102,20 @@ export default {
   font-size: 20px;
 }
 
+.multiselect {
+  font-family: 'Oswald', sans-serif;
+  letter-spacing: 0.5px;
+}
+
 img {
   height: 380px;
   width: 380px;
 }
 form {
   font-family: 'Oswald', sans-serif;
-  font-size: 22px;
+  font-size: 16px;
   letter-spacing: 0.5px;
+  color: #35495e;
 }
 button {
   text-decoration: underline;
