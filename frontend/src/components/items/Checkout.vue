5a759bb3d6f766cdba3de1922f57076b37aabd17
Claire <smith.clairelouise@gmail.com>
2020-04-10 17:30:26 +0100

    checkout styled

--- frontend/src/components/items/Checkout.vue
+++ frontend/src/components/items/Checkout.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="checkout">
-    <h1>Checkout</h1>
-    <router-link to="/basket">Back to Basket</router-link>
-    <h2>Total: £{{ total }}</h2>
-    <button @click="makePayment">Make Payment</button>
+  <div id="checkout" class="flex flex-col items-center">
+    <h1 class="animated zoomInDown checkout-title text-6xl tracking-wide text-orange-600 mt-10 mb-10">Checkout</h1>
+    <h2 class="total">Total: £{{ total }}</h2>
+    <div class="flex justify-center mt-6 w-full"><button class="w-1/4 button bg-red-100" @click="makePayment">Make Payment</button></div>
+    <div class="flex justify-center mt-3 w-full"><router-link class="flex justify-center w-full" to="/basket"><button class="w-1/4 button bg-red-100">Back to Basket</button></router-link></div>
   </div>
 </template>
 <script>
@@ -53,5 +53,22 @@ export default {
 }
 </script>
 <style scoped>
-
+.checkout-title {
+  font-family: 'Pacifico', cursive;
+  font-size: 80px;
+}
+.button {
+  margin-top: 5px;
+  padding: 5px 8px;
+  box-shadow: 1px 1px 10px 1px gray;
+  font-family: 'Oswald', sans-serif;
+}
+.button:hover {
+  transform: scale(0.98);
+  color: #dd6b33;
+}
+.total {
+  font-family: 'Oswald', sans-serif;
+  font-size: 20px;
+}
 </style>
