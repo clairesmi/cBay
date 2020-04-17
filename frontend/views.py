333db68502df184de02f84d54980fb84b65be383
Claire <smith.clairelouise@gmail.com>
2020-04-17 11:35:05 +0100

    ready for deployment

--- frontend/views.py
+++ frontend/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse, HttpResponseNotFound
 # Home view opens the index.html file located in the dist folder and sends it back to the client
 class Home(View):
     def get(self, _request): # underscore - request can still be passed to the function even if it is not used
-        with open(os.path.join(os.path.dirname(__file__), 'dist', 'index.html')) as file:
+        with open(os.path.join(os.path.dirname(__file__), 'dist', 'static', 'index.html')) as file:
             return HttpResponse(file.read())
 
 
@@ -16,7 +16,7 @@ class Home(View):
 class Assets(View):
 
     def get(self, _request, filename):
-        path = os.path.join(os.path.dirname(__file__), 'dist', filename)
+        path = os.path.join(os.path.dirname(__file__), 'dist', 'static', filename)
 
         if os.path.isfile(path):
             with open(path, 'rb') as file:
